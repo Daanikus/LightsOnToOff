4c08d2eab9403fb6c483b327a7b7ddac100b38d4
daanikus <daanikus@gmail.com>
2018-04-18 16:49:38 +1200

    Initial commit

---
+++ lightson/LightsOnApp.java
@@ -0,0 +1,13 @@
+package lightson;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class LightsOnApp {
+
+    public static void main(String[] args) {
+        LightsOn lo = new LightsOn();
+        lo.solve();
+    }
+}
---
+++ testversion/LightsOnApp.java
@@ -0,0 +1,10 @@
+package lightson;
+
+public class LightsOnApp {
+
+    public static void main(String[] args) {
+        LightsOn lo = new LightsOn();
+        lo.print();
+        
+    }
+}
